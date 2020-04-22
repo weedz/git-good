03ebc819f0824a46618faba598d8b04f2c69a87a
Linus Björklund <weedzcokie@gmail.com>
2020-04-22 21:04:55 +0200

    Initial electron app

---
+++ webpack.renderer.config.js
@@ -0,0 +1,24 @@
+const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+// rules.push({
+//   test: /\.tsx?$/,
+//   exclude: /node_modules/,
+//   use: {
+//       loader: 'babel-loader'
+//   }
+// });
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+  },
+};
