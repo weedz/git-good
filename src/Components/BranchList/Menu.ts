bab477e3c8e4b06f5c695bdd36d2c50958387550
Linus Björklund <weedzcokie@gmail.com>
2020-06-25 17:26:35 +0200

    refactor BranchList

---
+++ src/Components/BranchList/Menu.ts
@@ -0,0 +1,114 @@
+import { remote } from "electron";
+import { contextMenuState, checkoutBranch } from "src/Data/Renderer/store";
+
+const { Menu, MenuItem } = remote;
+
+const originMenu = new Menu();
+originMenu.append(new MenuItem({
+    label: 'Fetch...',
+    click() {
+        console.log("fetch");
+        return "fetch";
+    }
+}));
+
+const remoteMenu = new Menu();
+remoteMenu.append(new MenuItem({
+    label: 'Checkout...',
+    click() {
+        console.log("Checkout");
+    }
+}));
+remoteMenu.append(new MenuItem({
+    label: 'Delete...',
+    click() {
+        console.log("Delete");
+    }
+}));
+remoteMenu.append(new MenuItem({
+    label: 'Rebase...',
+    click() {
+        console.log("Rebase");
+    }
+}));
+remoteMenu.append(new MenuItem({
+    label: 'Merge...',
+    click() {
+        console.log("Merge");
+    }
+}));
+
+const localMenu = new Menu();
+localMenu.append(new MenuItem({
+    label: 'Checkout...',
+    click() {
+        checkoutBranch(contextMenuState.data.dataset.ref);
+    }
+}));
+localMenu.append(new MenuItem({
+    label: 'Create new branch...',
+    click() {
+        console.log("Create new branch");
+    }
+}));
+localMenu.append(new MenuItem({
+    label: 'Delete...',
+    click() {
+        console.log("Delete");
+    }
+}));
+localMenu.append(new MenuItem({
+    label: 'Rebase...',
+    click() {
+        console.log("Rebase");
+    }
+}));
+localMenu.append(new MenuItem({
+    label: 'Merge...',
+    click() {
+        console.log("Merge");
+    }
+}));
+
+const headMenu = new Menu();
+headMenu.append(new MenuItem({
+    label: 'Push...',
+    click() {
+        console.log("Push");
+    }
+}));
+headMenu.append(new MenuItem({
+    label: 'Pull...',
+    click() {
+        console.log("Pull");
+    }
+}));
+
+export function showOriginMenu(e: any) {
+    e.preventDefault();
+    contextMenuState.data = e.currentTarget;
+    originMenu.popup({
+        window: remote.getCurrentWindow()
+    });
+}
+export function showRemoteMenu(e: any) {
+    e.preventDefault();
+    contextMenuState.data = e.currentTarget;
+    remoteMenu.popup({
+        window: remote.getCurrentWindow()
+    });
+}
+export function showLocalMenu(e: any) {
+    e.preventDefault();
+    contextMenuState.data = e.currentTarget;
+    localMenu.popup({
+        window: remote.getCurrentWindow()
+    });
+}
+export function showHeadMenu(e: any) {
+    e.preventDefault();
+    contextMenuState.data = e.currentTarget;
+    headMenu.popup({
+        window: remote.getCurrentWindow()
+    });
+}
---
+++ src/Components/BranchList/Utils.tsx
@@ -0,0 +1,139 @@
+import { h } from "preact";
+import { BranchObj, BranchesObj } from "src/Data/Actions";
+import { Link } from "@weedzcokie/router-tsx";
+
+export type BranchTree = {
+    subtree?: {
+        [path: string]: BranchTree
+    }
+    items?: Array<{
+        name: string
+        ref: BranchObj
+    }>
+};
+
+export function BranchAheadBehind(ref: BranchObj) {
+    let aheadbehind = [];
+    if (ref.status) {
+        if (ref.status.ahead) {
+            aheadbehind.push(<span>{ref.status.ahead}&uarr;</span>);
+        }
+        if (ref.status.behind) {
+            aheadbehind.push(<span>{ref.status.behind}&darr;</span>);
+        }
+    }
+    return aheadbehind;
+}
+
+export function toggleTreeItem(e: any) {
+    e.preventDefault();
+    const parent = e.target.parentNode;
+    if (parent.classList.contains("open")) {
+        parent.classList.remove("open");
+    } else {
+        parent.classList.add("open");
+    }
+    return false;
+}
+
+export function branchTree(branches: BranchTree, contextMenuCb?: (event: any) => void, dblClickHandle?: (event: any) => void) {
+    const items = [];
+    if (branches.subtree) {
+        for (const item of Object.keys(branches.subtree)) {
+            const children = branchTree(branches.subtree[item], contextMenuCb, dblClickHandle);
+            items.push(
+                <li class="sub-tree" key={item}>
+                    <a href="#" onClick={toggleTreeItem}>{item}</a>
+                    {children}
+                </li>
+            );
+        }
+    }
+    if (branches.items) {
+        for (const branch of branches.items) {
+            items.push(
+                <li key={branch.ref.headSHA}>
+                    <Link data-ref={branch.ref.name} onDblClick={dblClickHandle} onContextMenu={contextMenuCb} activeClassName="selected" href={`/branch/${encodeURIComponent(branch.ref.name)}`}>
+                        {branch.name}&nbsp;{BranchAheadBehind(branch.ref)}
+                    </Link>
+                </li>
+            );
+        }
+    }
+    return (
+        <ul class="tree-list block-list">
+            {items}
+        </ul>
+    );
+}
+export function listRemotes(branches: BranchTree, originContextMenuCb: (event: any) => void, contextMenuCb: (event: any) => void) {
+    if (!branches.subtree) {
+        return;
+    }
+    const items = [];
+    for (const item of Object.keys(branches.subtree)) {
+        const children = branchTree(branches.subtree[item], contextMenuCb);
+        items.push(
+            <li class="sub-tree">
+                <a onContextMenu={originContextMenuCb} href="#" onClick={toggleTreeItem}>{item}</a>
+                {children}
+            </li>
+        );
+    }
+    return (
+        <ul class="tree-list block-list">
+            {items}
+        </ul>
+    )
+}
+
+export function transformToBranchTree(branches: BranchObj[]) {
+    let root: BranchTree = {};
+    for (const branch of branches.sort((a,b) => a.normalizedName.localeCompare(b.normalizedName))) {
+        const paths = branch.normalizedName.split("/");
+
+        let tree = root;
+        while (paths.length > 1) {
+            const path = paths.shift() || "";
+            if (!tree.subtree) {
+                tree.subtree = {};
+            }
+            if (!tree.subtree[path]) {
+                tree.subtree[path] = {};
+            }
+            tree = tree.subtree[path];
+        }
+        if (!tree.items) {
+            tree.items = [];
+        }
+        tree.items.push({
+            name: paths[0],
+            ref: branch,
+        });
+    }
+    return root;
+}
+
+export function getBranchTree(branches: BranchesObj) {
+    return {
+        local: transformToBranchTree(branches.local),
+        remote: transformToBranchTree(branches.remote),
+        tags: {
+            items: branches.tags.map((tag: BranchObj) => ({
+                name: tag.normalizedName,
+                ref: {
+                    name: tag.name
+                }
+            })).sort( (a,b) => a.name.localeCompare(b.name))
+        } as BranchTree
+    };
+}
+
+export function filterBranches(branches: BranchesObj, filter: (value: BranchObj) => boolean)
+{
+    return {
+        remote: branches.remote.filter(filter),
+        local: branches.local.filter(filter),
+        tags: branches.tags.filter(filter),
+    };
+}
