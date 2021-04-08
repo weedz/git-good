3523b3bf4ecfb15cfd59bcc713359dccc817a22d
Linus Björklund <weedzcokie@gmail.com>
2021-04-08 15:29:06 +0200

    Use Remote.rename()
    Could not find this in the documentation for nodegit, but it is available from libgit2.

--- src/index.ts
+++ src/index.ts
@@ -415,14 +415,14 @@ const eventMap: {
             dialog.showErrorBox("Failed to edit remote", "Invalid name");
             return {result: false};
         }
-        // When we rename a remote, we need to remove the old remote and then
-        // create a new remote with the new name
+
         if (data.oldName !== data.name) {
-            await Remote.delete(repo, data.oldName);
-            await Remote.create(repo, data.name, data.pullFrom);
-        } else {
-            Remote.setUrl(repo, data.name, data.pullFrom);
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            await Remote.rename(repo, data.oldName, data.name);
         }
+
+        Remote.setUrl(repo, data.name, data.pullFrom);
 
         if (data.pushTo) {
             Remote.setPushurl(repo, data.name, data.pushTo);
