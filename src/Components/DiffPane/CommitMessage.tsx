e73fd8919777654ead598c78664140fff88d9987
Linus Björklund <weedzcokie@gmail.com>
2021-04-09 15:43:25 +0200

    Change format of date in commit view

--- src/Components/DiffPane/CommitMessage.tsx
+++ src/Components/DiffPane/CommitMessage.tsx
@@ -1,9 +1,11 @@
 import { h } from "preact";
 import { CommitObj } from "src/Data/Actions";
+import { formatTimeAgo } from "src/Data/Utils";
 import { GlobalLinks } from "src/Data/Renderer/store";
 import Link from "../Link";
 
 export default function CommitMessage(props: {commit: CommitObj}) {
+    const commitDate = new Date(props.commit.date * 1000);
     return (
         <div>
             <h4>{props.commit.sha}</h4>
@@ -13,7 +15,7 @@ export default function CommitMessage(props: {commit: CommitObj}) {
                     {props.commit.parents.map(parent => <li><Link type="commits" selectTarget={GlobalLinks.commits[parent.sha]}>{parent.sha.substring(0,7)}</Link></li>)}
                 </ul>
             </p>
-            <p className="date">Date: {new Date(props.commit.date * 1000).toLocaleString()}</p>
+            <p className="date">Date: {commitDate.toISOString()}<br />({formatTimeAgo(commitDate)})</p>
             {props.commit.date !== props.commit.authorDate && <p className="date">Authored: {new Date(props.commit.authorDate * 1000).toLocaleString()}</p>}
             <p className="author">author: {props.commit.author.name} &lt;{props.commit.author.email}&gt;</p>
             {props.commit.committer.email !== props.commit.author.email && <p className="author">commiter: {props.commit.committer.name} &lt;{props.commit.committer.email}&gt;</p>}
