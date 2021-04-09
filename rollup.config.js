9e3a37f748ee1c6fe74067cdf556002fa3168158
Linus Björklund <weedzcokie@gmail.com>
2021-04-09 13:29:57 +0200

    Add rollup-plugin-visualizer

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,43 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 const production = !process.env.ROLLUP_WATCH;
 
 const extensions = [".tsx", ".ts"];
 
+const external = [
+    "nodegit",
+    "path",
+    "fs",
+    "child_process",
+    "electron",
+    "@electron/remote"
+];
+
+const plugins = [
+    typescript(),
+    resolve({
+        extensions
+    }),
+    postcss({
+        minimize: true
+    }),
+    production && terser(),
+    // visualizer(),
+];
+
 export default {
-    external: [
-        "nodegit",
-        "path",
-        "fs",
-        "child_process",
-        "electron",
-        "@electron/remote"
-    ],
+    external,
     input: ["src/renderer.tsx", "src/index.ts"],
     output: {
         sourcemap: !production,
         format: "cjs",
         dir: "dist",
     },
-    plugins: [
-        typescript(),
-        resolve({
-            extensions
-        }),
-        postcss({
-            minimize: true
-        }),
-        production && terser(),
-    ],
+    plugins,
     watch: {
         clearScreen: false
     }
