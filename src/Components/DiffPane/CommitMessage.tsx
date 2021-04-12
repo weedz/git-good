9009a3216834c0100be1ad24dd7bebfb9e5b0e98
Linus Björklund <weedzcokie@gmail.com>
2021-04-12 07:51:46 +0200

    Show commit date without milliseconds

--- src/Components/DiffPane/CommitMessage.tsx
+++ src/Components/DiffPane/CommitMessage.tsx
@@ -6,6 +6,7 @@ import Link from "../Link";
 
 export default function CommitMessage(props: {commit: CommitObj}) {
     const commitDate = new Date(props.commit.date * 1000);
+    const authorDate = props.commit.date !== props.commit.authorDate ? new Date(props.commit.authorDate * 1000) : commitDate;
     return (
         <div>
             <h4>{props.commit.sha}</h4>
@@ -19,8 +20,8 @@ export default function CommitMessage(props: {commit: CommitObj}) {
                     {props.commit.parents.map(parent => <li><Link type="commits" selectTarget={GlobalLinks.commits[parent.sha]}>{parent.sha.substring(0,7)}</Link></li>)}
                 </ul>
             </p>
-            <p className="date">Date: {commitDate.toISOString()}<br />({formatTimeAgo(commitDate)})</p>
-            {props.commit.date !== props.commit.authorDate && <p className="date">Authored: {new Date(props.commit.authorDate * 1000).toLocaleString()}</p>}
+            <p className="date">Date: {commitDate.toISOString().substring(0, 19)}Z ({formatTimeAgo(commitDate)})</p>
+            {props.commit.date !== props.commit.authorDate && <p className="date">Authored: {authorDate.toISOString().substring(0, 19)}Z ({formatTimeAgo(authorDate)})</p>}
             <p className="author">author: {props.commit.author.name} &lt;{props.commit.author.email}&gt;</p>
             {props.commit.committer.email !== props.commit.author.email && <p className="author">commiter: {props.commit.committer.name} &lt;{props.commit.committer.email}&gt;</p>}
         </div>
