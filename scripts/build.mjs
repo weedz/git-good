800e159b08a1057ac0949af0a540ac2b0ffe5cf4
Linus Björklund <weedzcokie@gmail.com>
2022-11-10 00:32:56 +0100

    Add "jsdoc" type for `envPlugin` and fix some formatting

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,24 +1,26 @@
 import { execSync } from "node:child_process";
 import { build, analyzeMetafile } from "esbuild";
-let envPlugin = {
-    name: 'env',
+
+/** @type {import("esbuild").Plugin} */
+const envPlugin = {
+    name: "env",
     setup(build) {
         // Intercept import paths called "env" so esbuild doesn't attempt
         // to map them to a file system location. Tag them with the "env-ns"
         // namespace to reserve them for this plugin.
         build.onResolve({ filter: /^env$/ }, args => ({
             path: args.path,
-            namespace: 'env-ns',
+            namespace: "env-ns",
         }))
         
         // Load paths tagged with the "env-ns" namespace and behave as if
         // they point to a JSON file containing the environment variables.
-        build.onLoad({ filter: /.*/, namespace: 'env-ns' }, () => ({
+        build.onLoad({ filter: /.*/, namespace: "env-ns" }, () => ({
             contents: JSON.stringify({
                 buildDateTime: Date.now(),
                 lastCommit: execSync("git rev-parse HEAD").toString("utf8").trim(),
             }),
-            loader: 'json',
+            loader: "json",
         }))
     },
 }
