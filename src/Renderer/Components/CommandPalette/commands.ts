5fb6fdb941e2d4ce58c277d15c7fe9a065d5bf16
Linus Björklund <weedzcokie@gmail.com>
2025-04-14 21:44:16 +0200

    fix: Simplify command palette

--- src/Renderer/Components/CommandPalette/commands.ts
+++ src/Renderer/Components/CommandPalette/commands.ts
@@ -3,13 +3,13 @@ import { BranchFromType } from "../../../Common/Branch.js";
 import type { PromiseOrSync } from "../../../Common/TypeHelpers.js";
 import { basename } from "../../../Common/Utils.js";
 import {
-    openDialog_BranchFrom,
-    openDialog_Clone,
-    openDialog_compare,
-    openDialog_createTag,
-    openDialog_fileHistory,
-    openDialog_SetUpstream,
-    openDialog_viewCommit,
+  openDialog_BranchFrom,
+  openDialog_Clone,
+  openDialog_compare,
+  openDialog_createTag,
+  openDialog_fileHistory,
+  openDialog_SetUpstream,
+  openDialog_viewCommit,
 } from "../../Data/Dialogs.js";
 import { openFile, openSettings } from "../../Data/index.js";
 import { ipcGetData, ipcSendMessage } from "../../Data/IPC.js";
@@ -17,193 +17,109 @@ import { Store } from "../../Data/store.js";
 import { getType } from "../DiffPane/utility.js";
 
 export interface Command {
-    label: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data?: any;
-    details?: string;
-    action: () => PromiseOrSync<void | true | Command[]>;
-    focusAction?: () => PromiseOrSync<void>;
+  label: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data?: any;
+  details?: string;
+  action: () => PromiseOrSync<void | boolean | Command[]>;
+  focusAction?: () => PromiseOrSync<void>;
 }
 
 function openRecentRepositoryAction(this: Command) {
-    this.details && ipcSendMessage(IpcAction.OPEN_REPOSITORY, this.details);
+  this.details && ipcSendMessage(IpcAction.OPEN_REPOSITORY, this.details);
 }
 function checkoutBranchAction(this: Command) {
-    this.data && ipcSendMessage(IpcAction.CHECKOUT_BRANCH, this.data);
+  this.data && ipcSendMessage(IpcAction.CHECKOUT_BRANCH, this.data);
+}
+
+export function makeCommand(label: Command["label"], action: Command["action"], data?: any, details?: string, focusAction?: Command["focusAction"]): Command {
+  return {
+    label,
+    action,
+    data,
+    details,
+    focusAction,
+  };
 }
 
 export const commandPaletteCommandList: Command[] = [
-    // REPO
-    {
-        label: "Repo: Open repository...",
-        action() {
-            ipcSendMessage(IpcAction.REQUEST_OPEN_REPO, null);
-        },
-    },
-    {
-        label: "Repo: Clone repository...",
-        action() {
-            openDialog_Clone();
-        },
-    },
-    {
-        label: "Repo: Open recent repository",
-        async action() {
-            const recentRepositories = await ipcGetData(IpcAction.GET_RECENT_REPOSITORIES, null);
-            return recentRepositories.map(repoPath => ({
-                label: basename(repoPath),
-                details: repoPath,
-                action: openRecentRepositoryAction,
-            }));
-        },
-    },
-    {
-        label: "Repo: Fetch all",
-        action() {
-            ipcSendMessage(IpcAction.FETCH, null);
-        },
-    },
-    {
-        label: "Repo: Pull",
-        action() {
-            ipcSendMessage(IpcAction.PULL, null);
-        },
-    },
-    {
-        label: "Repo: Push",
-        action() {
-            ipcSendMessage(IpcAction.PUSH, null);
-        },
-    },
-    {
-        label: "Repo: File history...",
-        action() {
-            // TODO: Send data to main thread?
-            openDialog_fileHistory();
-        },
-    },
-    {
-        label: "Repo: Compare revisions...",
-        action() {
-            // TODO: Send data to main thread?
-            openDialog_compare();
-        },
-    },
-    {
-        label: "Repo: View commit...",
-        action() {
-            // TODO: Send data to main thread?
-            openDialog_viewCommit();
-        },
-    },
-    // Working directory
-    {
-        label: "Working directory: Stage file...",
-        async action() {
-            const unstagedChanges = await ipcGetData(IpcAction.GET_UNSTAGED_CHANGES, null);
-            return unstagedChanges.map(patch => ({
-                label: `[${getType(patch.status)}] ${patch.actualFile.path}`,
-                focusAction() {
-                    openFile({ workDir: true, patch, type: "unstaged" });
-                },
-                async action() {
-                    await ipcGetData(IpcAction.STAGE_FILE, patch.actualFile.path);
-                    return true;
-                },
-            }));
-        },
-    },
-    // BRANCH
-    {
-        label: "Branch: Set upstream...",
-        action() {
-            const local = Store.head?.name;
-            const remote = Store.head?.remote;
-            if (local) {
-                openDialog_SetUpstream(local, remote);
-            }
-        },
-    },
-    {
-        label: "Branch: Create new branch from HEAD...",
-        action() {
-            const headRef = Store.head?.name;
-            if (headRef) {
-                openDialog_BranchFrom(headRef, BranchFromType.REF);
-            }
-        },
-    },
-    {
-        label: "Branch: Checkout...",
-        action() {
-            // TODO: remote branches?
-            if (!Store.branches) {
-                return;
-            }
-            return Store.branches.local.map(branch => ({
-                label: branch.normalizedName,
-                data: branch.name,
-                action: checkoutBranchAction,
-            }));
-        },
-    },
-    // STASH
-    {
-        label: "Stash: Stash changes",
-        action() {
-            console.log("TODO: 'Stash changes'");
-        },
-    },
-    {
-        label: "Stash: Pop",
-        action() {
-            console.log("TODO: 'Stash: Pop'");
-        },
-    },
-    {
-        label: "Stash: Apply",
-        action() {
-            console.log("TODO: 'Stash: Apply'");
-        },
-    },
-    {
-        label: "Blame: File",
-        action() {
-            // TODO: return a list where the user can search for a file
-            console.log("TODO: 'Blame: File'");
-        },
-    },
-    {
-        label: "Tag: Create at HEAD...",
-        action() {
-            const headRef = Store.head?.name;
-            if (headRef) {
-                openDialog_createTag(headRef);
-            }
-        },
-    },
-    // Settings and misc.
-    {
-        label: "Open in Terminal",
-        action() {
-            ipcSendMessage(IpcAction.OPEN_IN_TERMINAL, null);
-        },
-    },
-    {
-        label: "Open in File Manager",
-        action() {
-            ipcSendMessage(IpcAction.OPEN_IN_FILE_MANAGER, null);
-        },
-    },
-    {
-        label: "Open preferences",
-        action() {
-            openSettings();
-        },
-    },
-    /**
-     * TOOD:
-     *      - Delete branch?
-     *      - Branch from ref/sha/commit?
-     */
+  // REPO
+  makeCommand("Repo: Open repository", () => { ipcSendMessage(IpcAction.REQUEST_OPEN_REPO, null) }),
+  makeCommand("Repo: Clone", () => { openDialog_Clone() }),
+  makeCommand("Repo: Open recent repository...", async () => {
+    const recentRepositories = await ipcGetData(IpcAction.GET_RECENT_REPOSITORIES, null);
+    return recentRepositories.map(repoPath => makeCommand(
+      basename(repoPath),
+      openRecentRepositoryAction,
+      undefined,
+      repoPath,
+    ));
+  }),
+  makeCommand("Repo: Fetch all", () => { ipcSendMessage(IpcAction.FETCH, null); }),
+  makeCommand("Repo: Pull", () => { ipcSendMessage(IpcAction.PULL, null); }),
+  makeCommand("Repo: Push", () => { ipcSendMessage(IpcAction.PUSH, null); }),
+  // TODO: Send data to main thread?
+  makeCommand("Repo: File history...", () => { openDialog_fileHistory(); }),
+  // TODO: Send data to main thread?
+  makeCommand("Repo: Compare revisions...", () => { openDialog_compare(); }),
+  // TODO: Send data to main thread?
+  makeCommand("Repo: View commit...", () => { openDialog_viewCommit(); }),
+  // Working directory
+  makeCommand("Working directory: Stage file...", async () => {
+    const unstagedChanges = await ipcGetData(IpcAction.GET_UNSTAGED_CHANGES, null);
+    return unstagedChanges.map(patch => makeCommand(
+      `[${getType(patch.status)}] ${patch.actualFile.path}`,
+      async () => {
+        await ipcGetData(IpcAction.STAGE_FILE, patch.actualFile.path);
+        return true;
+      },
+      undefined,
+      undefined,
+      () => openFile({ workDir: true, patch, type: "unstaged" })
+    ),
+    )
+  }),
+  // BRANCH
+  makeCommand("Branch: Set upstream...", () => {
+    const local = Store.head?.name;
+    const remote = Store.head?.remote;
+    if (local) {
+      openDialog_SetUpstream(local, remote);
+    }
+  }),
+  makeCommand("Branch: Create new branch from HEAD", () => {
+    const headRef = Store.head?.name;
+    if (headRef) {
+      openDialog_BranchFrom(headRef, BranchFromType.REF);
+    }
+  }),
+  makeCommand("Branch: Checkout...", () => {
+    // TODO: remote branches?
+    if (!Store.branches) {
+      return;
+    }
+    return Store.branches.local.map(branch => makeCommand(branch.normalizedName, checkoutBranchAction, branch.name));
+  }),
+  // STASH
+  makeCommand("Stash: Stash changes", () => console.log("TODO: 'stash changes'")),
+  makeCommand("Stash: Pop", () => console.log("TODO: 'stash: pop'")),
+  makeCommand("Stash: Apply", () => console.log("TODO: 'stash: apply'")),
+  // TODO: return a list where the user can search for a file
+  makeCommand("Blame: File", () => console.log("TODO: 'blame file'")),
+  // TAG
+  makeCommand("Tag: Create at HEAD...", () => {
+    const headRef = Store.head?.name;
+    if (headRef) {
+      openDialog_createTag(headRef);
+    }
+  }),
+  // Settings and misc.
+  makeCommand("Open in Terminal", () => { ipcSendMessage(IpcAction.OPEN_IN_TERMINAL, null); }),
+  makeCommand("Open in File Manager", () => { ipcSendMessage(IpcAction.OPEN_IN_FILE_MANAGER, null); }),
+  makeCommand("Open preferences", () => { openSettings(); }),
+  /**
+   * TOOD:
+   *      - Delete branch?
+   *      - Branch from ref/sha/commit?
+   */
 ];
